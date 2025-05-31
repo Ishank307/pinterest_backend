e3fd673594b10fbe7eea0b74ca9573dfaa3d0fa4
Ishank307 <ishankkumar307@gmail.com>
2025-05-31 16:40:23 +0530

    frontend

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 const userModel = require('./users');
 const postModel = require('./post');
+const passport = require('passport');
+const localStratergy=require('passport-local');
+passport.authenticate(new localStratergy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -23,36 +26,65 @@ router.get('/newUser', async function (req, res, next) {
 });
 
 
-router.get('/alluserposts', async function (req,res,next) {
+router.get('/alluserposts', async function (req, res, next) {
   let user = await userModel.findOne({
-    _id:"6838beccddc6e09da3d61fa8"
+    _id: "6838beccddc6e09da3d61fa8"
   }).populate('posts')
   res.send(user)
-  
+
 }),
 
-router.post('/register', function(req,res,next){
-  const userData= new userModel({
-username: req.body.username,
-      email: req.body.email,
-      fullName: req.body.fullName,
-  })
+
+router.get('/profile',isLoggedIn,function(req,res,next){
+  res.send('profile');
 })
+
+  router.post('/register', (req, res, next) => {
+    const { username, email, fullName } = req.body;
+    const userData = new userModel({ username, email, fullName });
+
+    userModel.register(userData, req.body.password).then(function () {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/profile');
+      })
+    })
+
+  });
+
+
+  router.post('/login', passport.authenticate('local',{
+    successRedirect: '/profile',
+    failureRedirect: '/'
+  }),function(req,res){});
+
+  router.get('/logout',(req,res,next)=>{
+    req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+  });
+
+  function isLoggedIn(req,res,next){
+    if (req.isAuthenticated()) return next();
+    res.redirect('/')
+
+  }
+
 
 
 router.get('/createPost', async function (req, res, next) {
-  let createdpost= await postModel.create(
+  let createdpost = await postModel.create(
     {
       postText: "kaise ho sab",
-      user:"6838beccddc6e09da3d61fa8",
+      user: "6838beccddc6e09da3d61fa8",
     });
-    let user= await userModel.findOne({});
-    user.posts.push(createdpost.id);
-    await user.save();
-    res.send("done");
+  let user = await userModel.findOne({});
+  user.posts.push(createdpost.id);
+  await user.save();
+  res.send("done");
 
 
-    // res.send(createdpost)
+  // res.send(createdpost)
 
 })
 
